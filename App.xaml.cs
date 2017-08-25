9ef535293f6a7ba2256b3f574f14ddabf06517ac
Pavel Khrapkin <pavel.khrapkin@gmail.com>
2017-08-25 17:47:45 +0300

    WPF and command line file path

---
+++ App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Windows;
+
+namespace IfcGuidRepair
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow wnd = new MainWindow();
+            if (e.Args.Length == 1)
+            {
+                wnd.path = e.Args[0];
+                if (!File.Exists(wnd.path)) wnd.Show();
+                wnd.HandleIfc();
+                Application.Current.Shutdown();
+            } 
+        }
+    }
+}
